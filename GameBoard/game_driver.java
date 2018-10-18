85b49463bef831611936002768d4ec3360ce240f
Unknown <frankie.dennehy@gmail.com>
2018-10-18 18:28:19 -0400

    Created the driver class
    this is just a skeleton of the main method that will be filled in later

---
+++ GameBoard/game_driver.java
@@ -0,0 +1,18 @@
+/**
+  * @author: Francine Dennehy
+  * @author: Alexandra Mullan
+  * @version: 1.0
+  * This class contains the main function that will start the entire game of
+  * Konane.
+  *
+ **/
+public class game_driver{
+
+/**
+  * this is the main method 
+ **/
+public static void main(void){
+  //call methods here
+}//end of main
+
+}//end of game_driver class
