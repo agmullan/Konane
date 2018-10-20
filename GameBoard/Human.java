9ebf4911aba4e70a99350b512b3291b8e0099cc2
Unknown <frankie.dennehy@gmail.com>
2018-10-20 18:59:36 -0400

    Merged in a way

---
+++ GameBoard/Human.java
@@ -0,0 +1,18 @@
+/**
+  * @author: Francine Dennehy
+  * @author: Alexandra Mullan
+  * @version: 1.0
+  * This class contains the methods to set up the artifical interegents.
+  *
+ **/
+public class Human extends Player{
+
+public Human(){
+}
+
+public char[][] takeTurn(){
+  char[][] ch = new char[8][8];
+  return ch;
+}
+
+}//end of Agent class
