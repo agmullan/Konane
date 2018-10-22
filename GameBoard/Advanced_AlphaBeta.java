fea0a0a0ed19a0db011b7916541f5b9e0e5a373d
Unknown <frankie.dennehy@gmail.com>
2018-10-22 18:26:08 -0400

    Added first stages of Advanced Alpha_Beta Pruning

--- GameBoard/Advanced_AlphaBeta.java
+++ GameBoard/Advanced_AlphaBeta.java
@@ -11,8 +11,9 @@
   * function Max-Value(state, alpha, beta) returns a utility Value
   *    if Terminal-Test(state) then return Utility(state)
   *    v <- -infin
+  *    if(e - 2 >= beta ) return e - 2
   *    for each a in Actions(state) do
-  *      v <- Max(v, Min-Value(Result(s,a), alpha, beta))
+  *      v <- Max(v, Min-Value(child, Max(alpha, e -2) , Min(beta, e + 2)))
   *      if v >= Beta then return v
   *      alpha <- MAX(alpha, v)
   *    return v
@@ -20,8 +21,9 @@
   * function Min-Value(state, alpha, beta) returns a utility Value
   *    if Terminal-Test(state) then return Utility(state)
   *    v <- +infin
+  *    if(e + 2 <= alpha ) return e - 2
   *    for each a in Actions(state) do
-  *      v <- Min(v, Max-Value(Result(s,a), alpha, beta))
+  *      v <- Min(v, Min-Value(child, Max(alpha, e - 2) , Min(beta, e + 2)))
   *      if v <= alpha then return v
   *      Beta <- MIN(Beta, v)
   *    return v
@@ -30,7 +32,7 @@
 import java.util.*;
 import java.math.*;
 
-public class AlphaBeta{
+public class Advanced_AlphaBeta{
 
 /*
  * We should have a list of Moves that will be judged based on how many steps the
@@ -44,13 +46,14 @@ public class AlphaBeta{
  * @version: 1.0
  * This function is called to perform Alphaq Beta Pruning
 **/
-public /*Move*/ void  Alpha_Beta_Search(GameBoard gb/* add */){
+public /*Move*/ void  Advanced_Alpha_Beta_Search(GameBoard gb/* add */){
   //this method is most likely going to change
-  
+
   int neg_infinity = Integer.MIN_VALUE;
   int pos_infinity = Integer.MAX_VALUE;
+  int temp = 0;
 
-  int v = Max_Value(gb, neg_infinity, pos_infinity);
+  int v = Max_Value(gb, neg_infinity, pos_infinity, temp);
 
   //needs to return a move for the player to perform
 
@@ -60,13 +63,18 @@ public /*Move*/ void  Alpha_Beta_Search(GameBoard gb/* add */){
  * @version 1
  * This finds the max value for the utility state.
 **/
-public int Max_Value(GameBoard gb, /* Moves ,*/ int alpha, int beta){
+public int Max_Value(GameBoard gb, /* Moves ,*/ int alpha, int beta, int util){
   int v = Integer.MIN_VALUE;
+
+  if((util - 2) >= beta){
+    return (util - 2 );
+  }//end if
 
   /* for */
   /* this loop would based on the list of states that could be possible */
-  int temp = 0; //this needs to change
-  v = Math.max(v, MIN(temp ,alpha, beta));
+  int child = 0; //this needs to change
+  v = Math.max(v, MIN(child , Math.max(alpha, (util - 2)), Math.min(beta, (util + 2))));
+
   if(v >= beta){
     return v;
   }
@@ -91,13 +99,17 @@ public int MAX(int a, int b, int c){
  * @version 1
  * This finds the min value for the utility state.
 **/
-public int Min_Value(GameBoard gb, /* Moves ,*/ int alpha, int beta){
+public int Min_Value(GameBoard gb, /* Moves ,*/ int alpha, int beta, int util){
   int v = Integer.MAX_VALUE;
+
+  if((util + 2) <= alpha){
+    return (util + 2 );
+  }//end if
 
   /* for */
   /* this loop would based on the list of states that could be possible */
-  int temp = 0; //this needs to change
-  v = Math.min(v, MAX(temp ,alpha, beta));
+  int child = 0; //this needs to change
+  v = Math.min(v, MAX(child , Math.max(alpha, (util - 2)), Math.min(beta, (util + 2)) ));
   if(v <= alpha){
     return v;
   }
